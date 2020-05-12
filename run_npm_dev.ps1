374685da0a2f4590c1abfada72a72667a258a20a
Florian <florian.von.bracht@apteco.de>
2020-05-12 12:44:15 +0200

    Helper powershell scripts to "npm start" and "npm dev"

---
+++ run_npm_dev.ps1
@@ -0,0 +1,18 @@
+################################################
+#
+# SCRIPT ROOT
+#
+################################################
+
+# Load scriptpath
+if ($MyInvocation.MyCommand.CommandType -eq "ExternalScript") {
+    $scriptPath = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
+} else {
+    $scriptPath = Split-Path -Parent -Path ([Environment]::GetCommandLineArgs()[0])
+}
+Set-Location -Path $scriptPath
+
+
+
+Set-Location -Path ".\oAuthBrowser"
+npm run dev
